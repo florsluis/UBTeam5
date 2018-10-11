6b36ac0fd7018a10dc3e40c1d99a57d6041fb2fd
Jorge Garcia-Rivera <jorge.garcia-rivera@p97.com>
2018-10-11 17:46:54 -0500

    tableview list

--- Exercise6Team5/Exercise6Team5/TableViewController.swift
+++ Exercise6Team5/Exercise6Team5/TableViewController.swift
@@ -7,89 +7,96 @@
 //
 
 import UIKit
+import MapKit
+
+struct Locations: Codable {
+    let company: String
+    let hq_latitude: Double
+    let hq_longitude: Double
+    
+    init() {
+        company = ""
+        hq_latitude = 0
+        hq_longitude = 0
+    }
+}
 
 class TableViewController: UITableViewController {
-
+    
+    var locations = [Locations]()
+    
+    var selectedLocation = Locations()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.title = "Locations"
+        let url = URL(string: "http://cpl.uh.edu/courses/ubicomp/fall2018/webservice/companies.json")
+        if url != nil {
+            getData(url: url!)
+        }
     }
+    
+    func getData(url: URL) {
 
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let locations = try jsonDecoder.decode(Array<Locations>.self, from: data)
+                    print(locations)
+                    self.locations = locations
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                } catch {
+                    print("Error trying to decode JSON object")
+                }
+                
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return locations.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let name = cell.viewWithTag(1) as! UILabel
+        
+        name.text = locations[indexPath.row].company
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedLocation = locations[indexPath.row]
+        self.performSegue(withIdentifier: "detailedSegue", sender: self)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "detailedSegue" {
+            let dvc = segue.destination as! ViewController
+            dvc.location = selectedLocation
+        }
     }
-    */
-
 }
--- Exercise6Team5/Exercise6Team5/ViewController.swift
+++ Exercise6Team5/Exercise6Team5/ViewController.swift
@@ -11,19 +11,17 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var location = Locations()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
